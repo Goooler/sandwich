1963f67903b7a6a5c254158c0fc4b1e8f173c188
Goooler <wangzongler@gmail.com>
2023-08-13 00:00:33 +0800

    Remove unnecessary repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,17 +14,15 @@
 
 pluginManagement {
   repositories {
-    gradlePluginPortal()
     google()
     mavenCentral()
-    maven(url = "https://plugins.gradle.org/m2/")
+    gradlePluginPortal()
   }
 }
 dependencyResolutionManagement {
   repositories {
     google()
     mavenCentral()
-    maven(url = "https://plugins.gradle.org/m2/")
   }
 }
 
